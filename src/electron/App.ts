703498e877af389a3d303af36df5b2504ddd2190
Fernando Zanutto <ferzanutto1999@gmail.com>
2021-11-18 23:35:40 -0300

    Arquivos que faltaram e gitignore

---
+++ src/electron/App.ts
@@ -0,0 +1,40 @@
+import { app, BrowserWindow } from 'electron';
+import { join } from 'path'
+
+class App {
+    static mainWindow: Electron.BrowserWindow | null;
+    static application: Electron.App;
+    static BrowserWindow: typeof BrowserWindow;
+    
+    private static onWindowAllClosed() {
+        if (process.platform !== 'darwin') {
+            App.application.quit();
+        }
+    }
+
+    private static onClose() {
+        App.mainWindow = null;
+    }
+
+    private static onReady() {
+        App.mainWindow = new App.BrowserWindow({ width: 800, height: 600 });
+        
+        const path = join('file://', __dirname, '/index.html')
+        App.mainWindow.loadURL(path);
+        
+        App.mainWindow.on('closed', App.onClose);
+        
+        App.application.on('activate', () => {
+            if (App.BrowserWindow.getAllWindows().length === 0) App.onReady()
+        })
+    }
+
+    static start(app: Electron.App, browserWindow: typeof BrowserWindow) {
+        App.BrowserWindow = browserWindow;
+        App.application = app;
+        App.application.on('window-all-closed', App.onWindowAllClosed);
+        App.application.on('ready', App.onReady);
+    }
+}
+
+App.start(app, BrowserWindow);
