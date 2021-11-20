c9f206f0de4073feb05aed6a022633eab10ad5cf
ernesto <evoliveira@inf.ufrgs.br>
2021-11-20 17:46:29 -0300

    Implement fotosensor

--- src/regatron-service/dispositivos/LuzInteligente/Fotosensor.ts
+++ src/regatron-service/dispositivos/LuzInteligente/Fotosensor.ts
@@ -2,7 +2,18 @@ import { Luminosidade } from "../../../model/Luminosidade";
 
 export class FotoSensor implements Sensor {
     getValorAtual(): number {
-        return Luminosidade.MEIA_LUZ
+        var luz : Luminosidade 
+        switch(Math.floor(Math.random()*3)){
+            case 0:
+                luz = Luminosidade.SOMBRA
+            case 1:
+                luz = Luminosidade.MEIA_LUZ
+            case 2:
+                luz = Luminosidade.SOL_PLENO
+            default:
+                luz = Luminosidade.SOMBRA //darkness prevails
+        }
+        return luz
     }
 
 }
