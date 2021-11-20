afa8280525515a9396f3b7aba9f664089954ea85
ricco <ricco23052000@gmail.com>
2021-11-20 20:21:29 -0300

    Attatch illumination to Vasos instances

--- src/services/RegatronService.ts
+++ src/services/RegatronService.ts
@@ -7,9 +7,11 @@ import { Termometro } from "../regatron-service/dispositivos/ACInteligente/Termo
 import { GerenciadorLuminosidade } from "../regatron-service/dispositivos/LuzInteligente/GerenciadorLuminosidade";
 import { Cobertor } from "../regatron-service/dispositivos/LuzInteligente/Cobertor";
 import { FotoSensor } from "../regatron-service/dispositivos/LuzInteligente/Fotosensor";
-//import { Lampada } from "../regatron-service/dispositivos/LuzInteligente/Lampada";
+import { Lampada } from "../regatron-service/dispositivos/LuzInteligente/Lampada";
 
 export class RegatronService {
+
+    //instanciando dispositivos de temperatura
     private arCondicionado1 = new ArCondicionado(20)
     private termometro1 = new Termometro
     private gerenciadorTemperatura1 = new GerenciadorTemperatura(this.termometro1, this.arCondicionado1)
@@ -21,7 +23,26 @@ export class RegatronService {
 
     private arCondicionado3 = new ArCondicionado(24)
     private termometro3 = new Termometro
-    private gerenciadorTemperatura3 = new GerenciadorTemperatura(this.termometro3, this.arCondicionado2)    
+    private gerenciadorTemperatura3 = new GerenciadorTemperatura(this.termometro3, this.arCondicionado2)
+    
+    //instanciando dispositivos de iluminacao
+    private cobertor1 = new Cobertor()
+    private fotoSensor1 = new FotoSensor()
+    private lampada1 = new Lampada()
+    private gerenciadorLuminosidade1 = new GerenciadorLuminosidade(this.fotoSensor1, this.lampada1, this.cobertor1)
+
+    private cobertor2 = new Cobertor()
+    private fotoSensor2 = new FotoSensor()
+    private lampada2 = new Lampada()
+    private gerenciadorLuminosidade2 = new GerenciadorLuminosidade(this.fotoSensor2, this.lampada2, this.cobertor2)
+
+    private cobertor3 = new Cobertor()
+    private fotoSensor3 = new FotoSensor()
+    private lampada3 = new Lampada()
+    private gerenciadorLuminosidade3 = new GerenciadorLuminosidade(this.fotoSensor3, this.lampada3, this.cobertor3)
+
+    //instanciando dispositivos de regacao
+ 
 
     plantas: Planta[] = [
         new Planta({id: 1, nomeCientifico: "PLANTUS DELICIUS", nomeUsual: "GOSTOSA", luminosidade: Luminosidade.MEIA_LUZ, quantidadeAguaPadrao: 200, temperaturaMaximaPadrao: 74, temperaturaMinimaPadrao: 0}),
@@ -29,9 +50,9 @@ export class RegatronService {
         
     ]
     vasos: Vaso[] = [
-        new Vaso({descricao: "Vaso da cozinha", dispositivos: [this.gerenciadorTemperatura1], id: 1, planta: this.plantas[0]}),
-        new Vaso({descricao: "Vaso sanitário", dispositivos: [this.gerenciadorTemperatura2], id: 2, planta: this.plantas[0]}),
-        new Vaso({descricao: "Vaso da sacada", dispositivos: [this.gerenciadorTemperatura3], id: 3, planta: this.plantas[1]}),
+        new Vaso({descricao: "Vaso da cozinha", dispositivos: [this.gerenciadorTemperatura1, this.gerenciadorLuminosidade1], id: 1, planta: this.plantas[0]}),
+        new Vaso({descricao: "Vaso sanitário", dispositivos: [this.gerenciadorTemperatura2, this.gerenciadorLuminosidade2], id: 2, planta: this.plantas[0]}),
+        new Vaso({descricao: "Vaso da sacada", dispositivos: [this.gerenciadorTemperatura3, this.gerenciadorLuminosidade3], id: 3, planta: this.plantas[1]}),
     ]
 
     public listPlantas(): Planta[] {
