2e117346836715d3d49128f2c28a62f7c2eb901b
Fernando Zanutto <ferzanutto1999@gmail.com>
2021-11-20 20:01:15 -0300

    Lógica de iniciar controller e view do Vaso e ajuste model do vaso

--- src/model/Vaso.model.ts
+++ src/model/Vaso.model.ts
@@ -11,8 +11,8 @@ interface VasoDTO {
     temperaturaMinima?: number;
     temperaturaMaxima?: number;
     luminosidade?: Luminosidade;
-    planta: Planta;
-    dispositivos: Dispositivo[]
+    planta?: Planta;
+    dispositivos?: Dispositivo[]
 }
 export class Vaso {
     public id: number;
@@ -21,7 +21,7 @@ export class Vaso {
     public temperaturaMinima: number;
     public temperaturaMaxima: number;
     public luminosidade: Luminosidade;
-    public planta: Planta;
+    public planta: Planta | null;
     public dispositivos: Dispositivo[] = []
 
     constructor(
@@ -36,12 +36,12 @@ export class Vaso {
     ) { 
         this.id = id;
         this.descricao = descricao;
-        this.quantidadeAgua = quantidadeAgua || planta.quantidadeAguaPadrao;
-        this.temperaturaMaxima = temperaturaMaxima || planta.temperaturaMaximaPadrao;
-        this.temperaturaMinima = temperaturaMinima || planta.temperaturaMinimaPadrao;
-        this.luminosidade = luminosidade || planta.luminosidade;
-        this.planta = planta;
-        this.dispositivos = dispositivos;
+        this.quantidadeAgua = quantidadeAgua || planta?.quantidadeAguaPadrao || 0;
+        this.temperaturaMaxima = temperaturaMaxima || planta?.temperaturaMaximaPadrao || 0;
+        this.temperaturaMinima = temperaturaMinima || planta?.temperaturaMinimaPadrao || 0;
+        this.luminosidade = luminosidade || planta?.luminosidade || Luminosidade.MEIA_LUZ;
+        this.planta = planta || null;
+        this.dispositivos = dispositivos || [];
         this.configurarDispositivos()
     }
 
