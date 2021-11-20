76e5bba168dfbb5081a8f382d5d5dfda827117c3
ricco <ricco23052000@gmail.com>
2021-11-20 18:55:34 -0300

    Attatch Dispositivo to Vaso with RegatronService

--- src/services/RegatronService.ts
+++ src/services/RegatronService.ts
@@ -1,15 +1,22 @@
 import { Luminosidade } from "../model/Luminosidade";
 import { Planta } from "../model/Planta.model";
 import { Vaso } from "../model/Vaso.model";
+import { GerenciadorTemperatura } from "../regatron-service/dispositivos/ACInteligente/GerenciadorTemperatura";
+import { ArCondicionado } from "../regatron-service/dispositivos/ACInteligente/ArCondicionado";
+import { Termometro } from "../regatron-service/dispositivos/ACInteligente/Termometro";
 
 export class RegatronService {
+    private arCondicionado = new ArCondicionado(20)
+    private termometro = new Termometro
+    private gerenciadorTemperatura = new GerenciadorTemperatura(this.termometro, this.arCondicionado)
+
     plantas: Planta[] = [
         new Planta({id: 1, nomeCientifico: "PLANTUS DELICIUS", nomeUsual: "GOSTOSA", luminosidade: Luminosidade.MEIA_LUZ, quantidadeAguaPadrao: 200, temperaturaMaximaPadrao: 74, temperaturaMinimaPadrao: 0}),
         new Planta({id: 2, nomeCientifico: "PLANTUS NOTDELICIUS", nomeUsual: "RUIM", luminosidade: Luminosidade.SOL_PLENO, quantidadeAguaPadrao: 200, temperaturaMaximaPadrao: 220, temperaturaMinimaPadrao: 30})
         
     ]
     vasos: Vaso[] = [
-        new Vaso({descricao: "Vaso da cozinha", dispositivos: [], id: 1, planta: this.plantas[0]}),
+        new Vaso({descricao: "Vaso da cozinha", dispositivos: [this.gerenciadorTemperatura], id: 1, planta: this.plantas[0]}),
         new Vaso({descricao: "Vaso sanitário", dispositivos: [], id: 2, planta: this.plantas[0]}),
         new Vaso({descricao: "Vaso da sacada", dispositivos: [], id: 3, planta: this.plantas[1]}),
     ]
