c4819f3ebb941a679974c5e3a93d33b9705c1902
ernesto <evoliveira@inf.ufrgs.br>
2021-11-23 20:08:53 -0300

    Remove unused classes

--- src/Main.ts
+++ src/Main.ts
@@ -1,18 +1,15 @@
 import { BaseController } from "./controller/BaseController";
 import { HomeController } from "./controller/Home.controller";
 import { PlantaController } from "./controller/Planta.controller";
-import { TestController } from "./controller/Test.controller";
 import { RegatronService } from "./services/RegatronService";
 import { VasoController } from "./controller/Vaso.controller";
 import { HomeView } from "./view/HomeView";
 import { PlantaView } from "./view/Planta.view";
-import { TestView } from "./view/TestView";
 import { VasoView } from "./view/Vaso.view";
 import { Regatron } from "./regatron-service/Regatron";
 
 export enum Pages {
     HOME,
-    TEST,
     PLANTA,
     VASO,
 }
@@ -25,7 +22,6 @@ export class NavigatorController {
 
     private static pagesMap: { [key in Pages]: () => BaseController<any> } = {
         [Pages.HOME]: () => new HomeController(new HomeView(), this.service),
-        [Pages.TEST]: () => new TestController(new TestView(), this.service),
         [Pages.PLANTA]: () =>
             new PlantaController(new PlantaView(), this.service),
         [Pages.VASO]: () => new VasoController(new VasoView(), this.service),
